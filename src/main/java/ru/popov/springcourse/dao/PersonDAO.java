a9b9e9717442ffecd4d4a79ccd16d64ae88ef6cb
Vladimir Popov <papoff8295@gmail.com>
2021-01-20 17:57:20 +0300

    lesson21

---
+++ src/main/java/ru/popov/springcourse/dao/PersonDAO.java
@@ -0,0 +1,31 @@
+package ru.popov.springcourse.dao;
+
+import org.springframework.stereotype.Component;
+import ru.popov.springcourse.models.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+    private static int PEOPLE_COUNT;
+    private List<Person> people;
+
+    {
+        people = new ArrayList<>();
+        people.add(new Person(++PEOPLE_COUNT, "Tom"));
+        people.add(new Person(++PEOPLE_COUNT, "Jerry"));
+        people.add(new Person(++PEOPLE_COUNT, "Pluto"));
+        people.add(new Person(++PEOPLE_COUNT, "Bob"));
+    }
+
+    public List<Person> index() {
+
+    return people;
+    }
+
+    public Person show(final int id) {
+        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+    }
+
+}
---
+++ src/main/java/ru/popov/springcourse/models/Person.java
@@ -0,0 +1,28 @@
+package ru.popov.springcourse.models;
+
+
+public class Person {
+    private int id;
+    private String name;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Person(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
